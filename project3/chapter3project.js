0b1423d67c59ca31078b037bc42f4d1593e4ffa2
mcurnal1 <mcurnal1@stumail.jccc.edu>
2025-10-05 02:32:59 -0500

    Create chapter3project.js

---
+++ project3/chapter3project.js
@@ -0,0 +1,12 @@
+var name = "Maryam Curnal";
+var desiredAnnualSalary = 1000000;
+var veteranStatus = false;
+const friends = ["lela", "luvi", "kena"];
+const desiredSalaryOfFriends = [100000, 100300, 100400];
+const newFriendObject = {
+  firstName: "kelly",
+  lastName: "james",
+  desiredSalary:100500,
+}
+console.log(name + desiredAnnualSalary + veteranStatus + friends + desiredSalaryOfFriends+newFriendObject.firstName+newFriendObject.lastName+newFriendObject.desiredSalary+newFriendObject);
+ 
