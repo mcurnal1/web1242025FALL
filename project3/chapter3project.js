da00da429bd83dfd38ccad7d7158f841cd515eaf
mcurnal1 <mcurnal1@stumail.jccc.edu>
2025-10-05 11:15:51 -0500

    Update chapter3project.js

--- project3/chapter3project.js
+++ project3/chapter3project.js
@@ -6,16 +6,7 @@ const desiredSalaryOfFriends = [100000, 100300, 100400];
 const newFriendObject = {
   firstName: "kelly",
   lastName: "james",
-  desiredSalary: 100500,
-};
-
-console.log(
-  name +
-  desiredAnnualSalary +
-  veteranStatus +
-  friends +
-  desiredSalaryOfFriends +
-  newFriendObject.firstName +
-  newFriendObject.lastName +
-  newFriendObject.desiredSalary +
-  JSON.stringify(newFriendObject)
+  desiredSalary:100500,
+}
+console.log(name + desiredAnnualSalary + veteranStatus + friends + desiredSalaryOfFriends+newFriendObject+newFriendObject);
+ 
